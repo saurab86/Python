361b190ea8234ebd126808e75dca06bde03bb842
saurab86 <karki.saurab18@gmail.com>
2021-08-23 17:57:05 +0545

    commit

--- 2. String in python/stringoperators.py
+++ 2. String in python/stringoperators.py
@@ -3,6 +3,14 @@
 string1="he's "
 string2="probably "
 string3="pining "
-string4="fjords "
+string4="fjords"
  
 print(string1 + string2 + string3 + string4)
+print("hello " * 5)  #Hello will be repeated 5 times.
+#print("Hello"* 5 +4) would not print Hello 9 times. I shows an error.
+#print("hello"(5+4)) would print Hello 9 times
+
+today = "friday"
+print("day" in today)       #TRUE
+print("fri" in today)       #TRUE
+print("parrot" in string4)  #TRUE
