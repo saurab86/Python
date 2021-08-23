1705b8b6501308e1778401234bbba51adaf1f151
saurab86 <karki.saurab18@gmail.com>
2021-08-23 17:48:21 +0545

    commit

---
+++ 2. String in python/stringoperators.py
@@ -0,0 +1,8 @@
+#Python 3 has 5 sequence types built in
+# They are the string type, list, tuple, range, bytes and bytearray
+string1="he's "
+string2="probably "
+string3="pining "
+string4="fjords "
+ 
+print(string1 + string2 + string3 + string4)
