e7d48743d9e0b7a938a70bcffd3fdae3ef4cffbe
saurab86 <karki.saurab18@gmail.com>
2021-08-23 20:44:29 +0545

    commit

---
+++ 2. String in python/replacementfeild.py
@@ -0,0 +1,12 @@
+age = 22
+print("My age is " + str(age))  #My age is 22. str replace int type to string
+
+#Print("My age  is " + age ) will give an error because in python string and integer cannot be concatinated
+
+print("I am {0} years old.".format(age)) #{0} is the replacement feild
+
+print("There are {0} days in {1}, {2}, {3}, {4}"
+     .format(31,"Jan","March","Sep", "Oct."))   #{0},{1},{2},{3},{4} are replacement feilds
+
+print("Give me {2} dollar {0} cents {1}".format(50,"okay!",100))  #replacement feilds are indexed sequentially ie. {2} bounds to 100
+                                                                  #{0} bounds to 50 and {1} bounds to okay
