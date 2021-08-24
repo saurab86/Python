afae2cd316fb4abb4e10a272c9e0663c0f38a64c
saurab86 <karki.saurab18@gmail.com>
2021-08-24 15:03:21 +0545

    commit

---
+++ 2. String in python/stringfoematting.py
@@ -0,0 +1,6 @@
+for i in range(1,13):
+    print("No. {0} squared is {1} and cubed is {2}".format(i, i ** 2, i**3))
+
+print("Pi is approximately {0:8f}".format(22/7))     # 3.142857  print up to 6 decimal  by default 
+print("Pi is approximately {0:8}".format(22/7))      # 3.142857142857143
+print("Pi approxmimately {0:8.50f}".format(22/7))    #  3.14285714285714279370154144999105483293533325195312 prints up to 50 decimal points
