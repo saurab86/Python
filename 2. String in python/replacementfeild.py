0503fa332e1fd338dc48881b786e6570108f0064
saurab86 <karki.saurab18@gmail.com>
2021-08-24 16:52:25 +0545

    commit

--- 2. String in python/replacementfeild.py
+++ 2. String in python/replacementfeild.py
@@ -9,4 +9,9 @@ print("There are {0} days in {1}, {2}, {3}, {4}"
      .format(31,"Jan","March","Sep", "Oct."))   #{0},{1},{2},{3},{4} are replacement feilds
 
 print("Give me {2} dollar {0} cents {1}".format(50,"okay!",100))  #replacement feilds are indexed sequentially ie. {2} bounds to 100
-                                                                  #{0} bounds to 50 and {1} bounds to okay
+     #{0} bounds to 50 and {1} bounds to okay
+print()
+name = "Red"
+age = 24 
+print(f"Name: {name} and Age: {age}")    #Here we need not use .format string replacement. We only use 'f' before double quotation.
+       #And string variable and integer variable needs to place {variable name or operation betn double quotation}
