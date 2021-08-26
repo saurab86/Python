9037cb62c8ea7a3f8f422d5a08de067135341804
saurab86 <karki.saurab18@gmail.com>
2021-08-26 18:11:36 +0545

    commit

--- 3. Program flow/guessinggame.py
+++ 3. Program flow/guessinggame.py
@@ -2,21 +2,34 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
-    guess=int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("You have not guessed correctly")
-
-elif guess > answer:
-    print("Please guess lower")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher.")
+    else:   # guess must be greater than answer
+        print("Please guess lower.")
     guess = int(input())
     if guess == answer:
-        print("Well done you have guessed it")
+        print("Well done, you guessed it.")
     else:
-        print("Sorry you have not guessed it correctly")
-
+        print("Sorry, you have not guessed correctly.")
 else:
-    print("You got it first time")
+    print("You got it first time.")
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess=int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("You have not guessed correctly")                       #THIS CODE IS CORRECT BUT LONG CODE
+
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done you have guessed it")
+#     else:
+#         print("Sorry you have not guessed it correctly")
+
+# else:
+#     print("You got it first time")
