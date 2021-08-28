c333b0eedc3a9d0a2d929be8a2f811a998ef69dd
saurab86 <karki.saurab18@gmail.com>
2021-08-28 14:42:25 +0545

    commit

---
+++ 3. Program flow/range.py
@@ -0,0 +1,15 @@
+for i in range(0,10):
+    print(i)            #prints the every value in new line starting from 0 to 9
+
+
+# for i in range(10,2):
+   # print(i)            #DOESN'T PRINT ANY THING BECAUSE i CAN'T BE INDEXED FROM 10 TO 2.
+print()
+
+for i in range(0,10,2):
+    print(i)          #prints every number with gap 2 betweem 0 to 10. i.e 0,2,4,6,8
+print()
+
+for i in range(10,0,-2):
+    print(i)            #print negative range values between 10 to 0 backward from 10 with 2 gap. i.e 10,8,6,4,2
+
