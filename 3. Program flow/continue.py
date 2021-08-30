3ac4e0fa63097c2291ee0d5052151dd228084018
saurab86 <karki.saurab18@gmail.com>
2021-08-30 18:34:48 +0545

    continue and break

--- 3. Program flow/continue.py
+++ 3. Program flow/continue.py
@@ -22,4 +22,7 @@ for index in range(len(shopping_list)):
     if shopping_list[index] =="spam":
         found_at =index
         break
-print("Item found at position {} ".format(found_at))
+if found_at is not None:
+ print("Item found at position {} ".format(found_at))
+else:
+    print("item {} not found".format(item_to_find))
