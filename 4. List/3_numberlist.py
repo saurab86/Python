b6f4b7ee7fbb6eee06517021119760c5c26b3932
saurab86 <karki.saurab18@gmail.com>
2021-09-04 18:32:18 +0545

    min and max

---
+++ 4. List/3_numberlist.py
@@ -0,0 +1,20 @@
+even = [2,4,6,8]
+odd = [1,3,5,7,9]
+
+print("The list of even number in list are: {}".format(even))
+print()
+print("The list of odd number in list are: {}".format(odd))
+print()
+
+print("The minimum even value in list is {}".format(min(even)))
+print("The maximum even value in list is {}".format(max(even)))
+print()
+
+print("The minimun odd value in list is {}".format(min(odd)))
+print("The maximum odd value in list is {}".format(max(odd)))
+print()
+
+print("The length of even numbers in list is {}".format(len(even)))
+print("The length of odd number in list is {}".format(len(odd)))
+
+print("Mississippi".count("iss"))
