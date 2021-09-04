704732496cd20b5ead5be59c545f709686e059a4
saurab86 <karki.saurab18@gmail.com>
2021-09-04 13:17:14 +0545

    commit

---
+++ 4. List/lists_intro.py
@@ -0,0 +1,10 @@
+computer_parts = ["computer","monitor","keyboard","mouse","mouse mat"]  #List is a sequenced order items represented in square brackets
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+print(computer_parts[0:3])   #List can also be sliced samme as strings.
+print(computer_parts[-1])    #Python idiom that returns the last items in the list
