f80684f60849d8cc1eac442674c696bfc4d8ac9a
saurab86 <karki.saurab18@gmail.com>
2021-09-04 17:59:26 +0545

    binding multiple name

--- 4. List/2_mutable.py
+++ 4. List/2_mutable.py
@@ -16,3 +16,10 @@ print(shoppinglist)            # O/P: ['milk', 'pasta', 'egg', 'spam', 'bread', 
 print(id(shoppinglist))        #give same id as above list because list are mutuable, Python can able to change the contents  of the list without creating new one.
 
 #STRING ARE 'IMMUTABLE'. WHEN WE TRY TO CHANGE A STRING, PYTHON CREATES THE NEW OBJECT -a NEW STRING - AND RE-ATTACHETED A NAME TO IT.
+print("*"*80)
+a = b = c = d = e = f = another_list  #Binding Multiple name to a List.
+print(a)
+print("Adding new item to a list that is 'Pasta'")
+b.append("Pasta")
+print(c)
+print(f)
