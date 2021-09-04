9cc3e59d89c16e01459fa970385463d61a98c287
saurab86 <karki.saurab18@gmail.com>
2021-09-04 17:50:41 +0545

    commit

---
+++ 4. List/2_mutable.py
@@ -0,0 +1,18 @@
+# A mutuable object is one whose value can be changed
+# Python has the following mutuable objects:
+#  list
+#  dict
+#  set
+#  bytearray
+
+shoppinglist= ["milk","pasta", "egg","spam","bread","rice"]
+
+another_list = shoppinglist
+print(id(shoppinglist))
+print(id(another_list))
+
+shoppinglist += ["Cookies"]    #Concatenate at the end of list
+print(shoppinglist)            # O/P: ['milk', 'pasta', 'egg', 'spam', 'bread', 'rice', 'Cookies']
+print(id(shoppinglist))        #give same id as above list because list are mutuable, Python can able to change the contents  of the list without creating new one.
+
+#STRING ARE 'IMMUTABLE'. WHEN WE TRY TO CHANGE A STRING, PYTHON CREATES THE NEW OBJECT -a NEW STRING - AND RE-ATTACHETED A NAME TO IT.
