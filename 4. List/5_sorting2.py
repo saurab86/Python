c94109898da10baaf0fa51556a0010eb5e20f6f4
saurab86 <karki.saurab18@gmail.com>
2021-09-07 12:41:09 +0545

    sorting list

---
+++ 4. List/5_sorting2.py
@@ -0,0 +1,12 @@
+pangram = "The quick brown fox jumps over the lazy dogs"
+
+letters=sorted(pangram) #sorts the item but duplicate the same items
+print(letters)
+print
+
+numbers=[2.3,4.5,8.7,3.1,9.2,1.6]
+sorted_numbers=sorted(numbers)
+print(sorted_numbers)
+
+#DIFFERENCE BETWEEN SORT AND SORTED IS THAT SORT METHOD DIRECTLY SORT THE DEFINED VARIALBLE 
+#BUT IN CASE OF SORTED, NEW VARIABLE IS DEFINED AS SORT THE ITEMS AAVAILABLE IN LIST
