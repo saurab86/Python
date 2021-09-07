72c46b249d19ace54cfcc9aa0b3e04c55e7e6fbd
Saurab Karki <68777795+saurab86@users.noreply.github.com>
2021-09-07 13:06:54 +0545

    Update 5_sorting2.py
    sorting

--- 4. List/5_sorting2.py
+++ 4. List/5_sorting2.py
@@ -7,6 +7,16 @@ print
 numbers=[2.3,4.5,8.7,3.1,9.2,1.6]
 sorted_numbers=sorted(numbers)
 print(sorted_numbers)
+print()
 
 #DIFFERENCE BETWEEN SORT AND SORTED IS THAT SORT METHOD DIRECTLY SORT THE DEFINED VARIALBLE 
 #BUT IN CASE OF SORTED, NEW VARIABLE IS DEFINED AS SORT THE ITEMS AAVAILABLE IN LIST
+
+#above there is jumps and dogs i.e 2 's' this case hase jumped and dogs only one 's'
+missing_letter=sorted("The quick brown fox jumped over the lazy dogs",key=str.casefold)  #key=str.casefold sort the Case_insensitive string 
+print(missing_letter)
+print()
+
+names=["Benzema","Bale","Hazard","isco","Velverde","Casemiro","Alaba","Nacho","Militao","carvajal","Courtious"]
+names.sort(key=str.casefold)
+print(names)
